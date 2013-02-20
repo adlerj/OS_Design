f9eb9e2308ffa530de2149366178df88f6671223
adlerj <jultimate123@gmail.com>
2013-02-20 16:47:51 -0500

    Cleaned and removed prints
    cleaner, only prints time

--- wtc_btproc.c
+++ wtc_btproc.c
@@ -20,29 +20,18 @@ void err_exit()
 	exit(1);
 }
 
-int main(int argc, char ** argv){
+//open matrix, get line and thread count
+FILE * openMatrix(char * fileName, int * threadCount, int * lineCount)
+{
+	FILE *file = fopen(fileName, "r"); 
 
-	struct timeval t0;
-	struct timeval t1;
+	fscanf(file, "%d", threadCount);   
+	fscanf(file, "%d", lineCount); 
+	return file;
+}
 
-	gettimeofday(&t0, 0);
-	
-	if(argc != 2){fprintf(stderr, "Invalid Arguments.  Usage: functions <filename>\n"); exit(3);}
-
-	int threadCount;
-	int lineCount;
-
-	FILE *file = fopen(argv[1], "r"); 
-
-	fscanf(file, "%d", &threadCount);   
-	fscanf(file, "%d", &lineCount);   
-/////////////////////////////////////////////////////////////////////////////////
-	int shm = shm_open("/myshm", O_RDWR | O_CREAT, S_IRWXU);
-	int size = (sizeof(int) * lineCount * lineCount) + (sizeof(sem_t)*5) + (sizeof(int)*(4 + 1 + lineCount));
-	printf("Size: %i\n", size);
-	ftruncate(shm, size);
-//////////////////////////////////////////////////////////////////////////////////
-	int * matrix = (int *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);		
+int readMatrix(FILE * file, int * matrix, int lineCount)
+{
 	int a;
 	int b;
 	while (!feof (file))
@@ -52,11 +41,33 @@ int main(int argc, char ** argv){
 		//printf ("%d %d\n", a, b);     
     }
 	fclose (file); 
-	////////////////////////////////////////////////////////////////////////////////////////////////////
+}
+
+void setQueue(int * queue, int lineCount){
+	int l;
+	int * pQueue = queue;
+	for(l = 0; l < lineCount; l++){
+		*pQueue = l;
+		pQueue++;
+	}
+	*pQueue=-1;
+}
+
+int allocShm(int lineCount, FILE * file, int size){
+	int shm = shm_open("/myshm", O_RDWR | O_CREAT, S_IRWXU);
+	ftruncate(shm, size);
+
+	return shm;	
+}
+
+void initShm(int shm, int lineCount, int threadCount, FILE * file, int size){
 	int semOffset = (sizeof(sem_t)/sizeof(int));
+
+	int * matrix = (int *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);		
+	readMatrix(file, matrix, lineCount);
+	
 	sem_t * matrixLock;
 	matrixLock = (sem_t *) (matrix + (lineCount*lineCount));
-	//printf("Matrix: %p, Matrix Lock: %p, mSize: %i\n", matrix, matrixLock, ((sizeof(int)*lineCount*lineCount)));
 	sem_init(matrixLock, 1, 1);
 
 	sem_t * cLock = (sem_t *) (matrixLock + semOffset);
@@ -78,107 +89,111 @@ int main(int argc, char ** argv){
 	sem_init(contLock, 1, 1);
 
 	int * queue = (int *) (contLock + semOffset);
-
-	int l;
-	int * pQueue = queue;
-	for(l = 0; l < lineCount; l++){
-		*pQueue = l;
-		pQueue++;
-	}
-	*pQueue=-1;
+	setQueue(queue, lineCount);	
 
 	sem_t * queueLock = (sem_t *) (queue + (lineCount+1));
 	sem_init(queueLock, 1, 1);	
 
 	int * qNum = (int *) (queueLock + semOffset);
 	*qNum = 0;
-	//////////////////////////////////////////////////////////////////////////////////////////////////
-	//printf("Number of processes: %i\nNumber of Lines: %i\n\n", threadCount, lineCount);
+}
 
-	printf("\nInitial matrix:\n\n");
-	print_matrix(matrix, lineCount);
-	printf("\n");
+void childCompute(int * matrix, sem_t * matrixLock, int k, sem_t * queueLock, int * queue, int * qNum, int lineCount){
+	int y;
+	y = queuePop(queueLock, queue, qNum);
+			
+	while(y!= -1)
+	{
+		sem_wait(matrixLock);			
+		int j = 0;
+		for(;j < lineCount; j++){
+			matrix[y + (j * (lineCount))] = matrix[y + (j * (lineCount))] || (matrix[y + (k * (lineCount))] && matrix[k + (j * (lineCount))]);
+		}
+		sem_post(matrixLock);	
+		
+		y = queuePop(queueLock, queue, qNum);
+	}			
+}
+
+int queuePop(sem_t * queueLock, int * queue, int * qNum){
+	sem_wait(queueLock);
+	int y;
+	y = queue[*qNum];
+	if(y!=-1){
+		(*qNum)++;
+	}
+	sem_post(queueLock);
+	return y;
+}
+
+int main(int argc, char ** argv){
+
+	struct timeval t0;
+	struct timeval t1;
+
+	gettimeofday(&t0, 0);
+	
+	if(argc != 2){fprintf(stderr, "Invalid Arguments.  Usage: functions <filename>\n"); exit(3);}
+
+	int threadCount;
+	int lineCount;
+
+	FILE * file = openMatrix(argv[1], &threadCount, &lineCount);
+	int shm;
+	
+	int size = (sizeof(int) * lineCount * lineCount) + (sizeof(sem_t)*5) + (sizeof(int)*(4 + 1 + lineCount));
+	shm = allocShm(lineCount, file, size);
+	initShm(shm, lineCount, threadCount, file, size);
+
+	int * matrix = (int *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);		
+	
+	//printf("\nInitial matrix:\n\n");
+	//print_matrix(matrix, lineCount);
+	//printf("\n");
 	
 	int pid = 1;
 	int * childPids = malloc(sizeof(int)*threadCount);
 	int myId = -1; //parentID is -1
 	int x = 0;
 	
+	/*Process Making*/
 	for(;(x < threadCount) && (pid != 0); x++){
 		if((pid=fork()) < 0){
-			printf("doodoo %i\n",pid);
 			err_exit();
 		}
 		if(pid != 0){ //Parent keep track of child pids
 			childPids[x] = pid;
 		}
 		else{ //child obtain identity
-			//printf("Thread %i started!\n", x);
 			myId = x;
 		}
 	}
 
-	int i = 0;
-	
-	/*if(pid!=0){
-		for(;i < threadCount; i++){printf("My ID is: %i\n", childPids[i]);}
-	}*/
-////////////////Prepared//Processes///////////////////////
-	
+	int semOffset = (sizeof(sem_t)/sizeof(int));
+
 	matrix = (int *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
-	matrixLock = (sem_t *) (matrix + (lineCount*lineCount));
+	sem_t * matrixLock = (sem_t *) (matrix + (lineCount*lineCount));
 	
-	cLock = (sem_t *) (matrixLock + semOffset);
-	count = (int *) (cLock + semOffset);
+	sem_t * cLock = (sem_t *) (matrixLock + semOffset);
+	int * count = (int *) (cLock + semOffset);
 	
-	bLock = (sem_t *) (count + 1);
-	bCount = (int *) (bLock + semOffset);
+	sem_t * bLock = (sem_t *) (count + 1);
+	int * bCount = (int *) (bLock + semOffset);
 
-	cont = (int *) (bCount + 1);
-	contLock = (sem_t *) (cont + 1);
+	int * cont = (int *) (bCount + 1);
+	sem_t * contLock = (sem_t *) (cont + 1);
 
-	queue = (int *) (contLock + semOffset);
-	queueLock = (sem_t *) (queue + (lineCount+1));
-	qNum = (int *) (queueLock + semOffset);
+	int * queue = (int *) (contLock + semOffset);
+	sem_t * queueLock = (sem_t *) (queue + (lineCount+1));
+	int * qNum = (int *) (queueLock + semOffset);
 
-	int rowsPer = lineCount/threadCount;
 
 	if(myId > -1){//Children
 		int nextFlip = 1;
 		int k = 0;
 		for(;k < lineCount; k++){
-			//int y = (myId * rowsPer);
-			//for(;y < ((myId+1) * rowsPer); y++){
-			int y = 0;;
-			sem_wait(queueLock);			
-			y = queue[*qNum];
-			if(y!=-1){
-				(*qNum)++;
-			}
-			//printf("(%i)Child %i handling line %i.\n", k, myId, y);
-			sem_post(queueLock);
-
-			while(y!= -1)
-			{
-				//printf("Child %i working on line %i of iteration %i\n", myId, y, k);
-				sem_wait(matrixLock);			
-				int j = 0;
-				for(;j < lineCount; j++){
-					matrix[y + (j * (lineCount))] = matrix[y + (j * (lineCount))] || (matrix[y + (k * (lineCount))] && matrix[k + (j * (lineCount))]);
-				}
-				sem_post(matrixLock);	
-
-				//printf("[K:%i|Y:%i]\n", k, y);
-
-				//pull from queue
-				sem_wait(queueLock);
-				y = queue[*qNum];
-				if(y!=-1){(*qNum)++;}
-				//printf("(%i)Child %i handling line %i.\n", k, myId, y);
-
-				sem_post(queueLock);
-			}
-			//ready for next k				
+			int y;
+			childCompute(matrix, matrixLock, k, queueLock, queue, qNum, lineCount);
 			sem_wait(bLock);
 			(*bCount)++;
 			sem_post(bLock);
@@ -188,7 +203,6 @@ int main(int argc, char ** argv){
 				tCont = *cont;
 				sem_post(contLock);
 			}while(tCont!= nextFlip);
-			//queue = queue - lineCount;
 			nextFlip = !nextFlip;
 		}
 		
@@ -228,16 +242,15 @@ int main(int argc, char ** argv){
 		free(childPids);
 
 		matrix = (int *) mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
-		printf("\nOutput Matrix:\n");
-		print_matrix(matrix, lineCount);
+		//printf("\nOutput Matrix:\n");
+		//print_matrix(matrix, lineCount);
 
 		munmap(matrix, size);
 		shm_unlink("/myshm");
 
 		gettimeofday(&t1, 0);
 		long elapsed = (t1.tv_sec-t0.tv_sec)*1000000 + t1.tv_usec-t0.tv_usec;
-		printf("\nTime(microseconds): %lu\n", elapsed);
-		//free(t0);free(t1);
+		printf("%lu\n", elapsed);
 	}
 	return 0;
 }
